af7937a0f7e168a93ee30ab8fdfbfb811413fe0d
Moscalu Stefan <94412232+Sm18990@users.noreply.github.com>
2023-08-13 13:27:59 +0300

    initial release

---
+++ main.py
@@ -0,0 +1,45 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
+print("Hello world!\n")
+
+print("Hello world!\n")
+
+def hello_function():
+    print("hello from my function")
+
+hello_function()
+
+def hello_function_name(name):
+    print("hello "+name)
+
+hello_function_name("stefan")
+
+def sum(a, b):
+    return a + b
+
+def div(a, b):
+    return a / b
+
+def multiply( a , b):
+    return a * b
+
+
+print( sum( 5, 10) )
+print( div( 10 , 2 ) )
+print( multiply(15 , 10 ) )
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
